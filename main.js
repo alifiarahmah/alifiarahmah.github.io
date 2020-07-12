d5e381a643e9f4d74a2405bb99cd441edcce0a9c
alifiarahmah <alifiarahmah03@gmail.com>
2020-07-12 19:45:37 +0700

    rombak2 v1.0

---
+++ main.js
@@ -0,0 +1,9 @@
+function dropdown() {
+    if(getElementById("dropdown").style.display == "none"){
+        //buka dd
+        getElementById("dropdown").style.display = "block";
+    } else{
+        //tutup dd
+        getElementById("dropdown").style.display = "none";
+    }
+}
--- wo_framework/main.js
+++
@@ -1,9 +0,0 @@
-function dropdown() {
-    if(getElementById("dropdown").style.display == "none"){
-        //buka dd
-        getElementById("dropdown").style.display = "block";
-    } else{
-        //tutup dd
-        getElementById("dropdown").style.display = "none";
-    }
-}
