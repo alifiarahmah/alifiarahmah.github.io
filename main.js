55572fae5bebfb2e69821587b18d49fbc90f3f91
alifiarahmah <alifiarahmah03@gmail.com>
2020-07-14 15:11:36 +0700

    Revert "remodel web sementara"
    This reverts commit c8ce5045242889a0178037adaee4d606d077d328.

--- main.js
+++ main.js
@@ -7,11 +7,3 @@ function dropdown() {
         getElementById("dropdown").style.display = "none";
     }
 }
-
-/*$(window).scroll(function() {
-    if ($(window).scrollTop() > 10) {
-        $('navbar').addClass('floatingNav');
-    } else {
-        $('navbar').removeClass('floatingNav');
-    }
-});*/
