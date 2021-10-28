e5e77d86cfd084616e8d8bced1299b1d75b522af
alifiarahmah <alifiarahmah03@gmail.com>
2021-10-28 23:17:52 +0700

    modified navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,21 +1,29 @@
 <template>
 	<nav class="block bg-transparent text-alipwhite h-1/12">
-		<div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+		<div class="max-w-7xl mx-auto">
 			<div class="relative flex items-center justify-between h-16">
 				<div class="flex-1 flex items-center justify-center">
 					<div class="flex-shrink-0 flex items-center">
-						<div class="hover:border-b-2 hover:border-white">
-							<a class="uppercase text-bold px-5 h-full" href="/">Home</a>
-						</div>
-						<div class="hover:border-b-2 hover:border-white">
-							<a class="uppercase text-bold px-5 h-full" href="/about">About</a>
-						</div>
-						<div class="hover:border-b-2">
-							<a class="uppercase text-bold px-5 h-full" href="/projects">Projects</a>
-						</div>
-						<div>
-							<a class="uppercase text-bold px-5 h-full" href="/blog">Blog</a>
-						</div>
+						<a class="uppercase text-bold px-5 h-full" href="/">
+							<div class="hover:bg-alipwhite hover:text-alipblue p-5 hover:border-white">
+								Home
+								</div>
+						</a>
+						<a class="uppercase text-bold px-5 h-full" href="/about">
+							<div class="hover:bg-alipwhite hover:text-alipblue p-5 hover:border-white">
+								About
+							</div>
+						</a>
+						<a class="uppercase text-bold px-5 h-full" href="/projects">
+							<div class="hover:bg-alipwhite hover:text-alipblue p-5 hover:border-white">
+								Projects
+							</div>
+						</a>
+						<a class="uppercase text-bold px-5 h-full" href="/blog">
+							<div class="hover:bg-alipwhite hover:text-alipblue p-5 hover:border-white">
+								Blog
+							</div>
+						</a>
 					</div>
 				</div>
 				</div>
