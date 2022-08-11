a903dd95ba695ce3e509af338cc27c2614ed4500
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-08-11 10:06:49 +0700

    Re-init to React

--- ceploi.sh
+++
@@ -1,8 +0,0 @@
-cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-git init
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:alifiarahmah/alifiarahmah.github.io.git master:gh-pages
-cd -
