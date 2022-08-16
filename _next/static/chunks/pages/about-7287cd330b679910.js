032d9af7f023a344a7d81b19e6aa87f6b2244004
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-08-16 22:43:18 +0700

    Updates

---
+++ _next/static/chunks/pages/about-7287cd330b679910.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{6772:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return u(5976)}])}},function(n){n.O(0,[875,572,379,976,297,929,774,888,179],(function(){return _=6772,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
