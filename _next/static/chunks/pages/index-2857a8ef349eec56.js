853cb72946da1497e35bf0eb6dd70d851792de22
alifiarahmah <28982967+alifiarahmah@users.noreply.github.com>
2022-08-17 00:10:48 +0000

    Deploying to gh-pages from  @ f77f6f05f41b1e3ed7400d277a6f154393ec8d93 🚀

---
+++ _next/static/chunks/pages/index-2857a8ef349eec56.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{6745:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(801)}])}},function(n){n.O(0,[875,572,379,976,297,759,774,888,179],(function(){return _=6745,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
