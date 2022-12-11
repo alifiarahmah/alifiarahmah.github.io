7d0ed95b8e54f5884a4abccaa810baf4e21f3aca
alifiarahmah <alifiarahmah03@gmail.com>
2022-12-11 14:55:40 +0700

    Updates

---
+++ _next/static/chunks/pages/about-11675bf837dd5c2f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{1929:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return u(1493)}])}},function(n){n.O(0,[875,154,572,379,203,769,774,888,179],(function(){return _=1929,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
---
+++ _next/static/chunks/pages/blog-6105f4f17610e2ca.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[195],{6053:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog",function(){return u(4298)}])}},function(n){n.O(0,[875,154,572,379,203,769,774,888,179],(function(){return _=6053,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
