08c22b5bc69b8e1d82e3bccd23e8c5b02e345ed4
Alifia Rahmah <alifiarahmah03@gmail.com>
2023-05-27 22:48:31 +0700

    Updates

--- _next/static/chunks/pages/index-f7608da036201bd0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{2285:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(6919)}])}},function(n){n.O(0,[875,154,572,379,203,769,774,888,179],(function(){return _=2285,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
