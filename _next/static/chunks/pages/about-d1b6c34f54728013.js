c7b10638f8033227f8f6ce4a1b36771ba0d5d433
alifiarahmah <alifiarahmah03@gmail.com>
2023-07-26 06:02:09 +0000

    Deploying to gh-pages from  @ 346541e292fda436440f8b6a4319e2f5050de2f4 🚀

--- _next/static/chunks/pages/about-d1b6c34f54728013.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{9509:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return u(3890)}])}},function(n){n.O(0,[372,351,219,292,353,612,774,888,179],(function(){return _=9509,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
